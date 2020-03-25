12a427ba23b5be395cf3e6f9bbbaefd47c0868a3
Wilson Chen <wilson.chen@codeimmersives.com>
2020-03-25 12:44:33 -0400

    finish

--- main.js
+++ main.js
@@ -1,9 +1,54 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+const isOdd = function(num){
+  return num % 2 === 1
+}
 
+const onlyOdds =function(array){
+  return array.filter(isOdd)
+}
 
+const isEven = function(num){
+  return num % 2 === 0
+}
 
+const onlyEvens = function(array){
+  return array.filter(isEven)
+}
+
+const isShort = function(str){
+  return str.length < 7
+}
+
+const shortNamesOnly = function(array){
+  return array.filter(isShort)
+}
+
+const isDName = function(name){
+  return name.startsWith('D')
+}
+
+const dNames = function(array){
+  return array.filter(isDName)
+}
+
+const containsLowercase = function(str){
+  return str !== str.toUpperCase()
+}
+
+const removeTrolls = function(array){
+  return array.filter(containsLowercase)
+}
+
+const isTriStateArea = function(str){
+  return str.endsWith('NY') || 
+  str.endsWith('CT') ||
+  str.endsWith('NJ')
+}
+const triStateAreaOnly = function(array){
+  return array.filter(isTriStateArea)
+}
 /*********************************
  * OUR CODE BELOW; DO NOT TOUCH! *
  *********************************/
